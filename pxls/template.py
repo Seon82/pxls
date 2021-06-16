966a8bb6af305ae2f154ee6f738031e075e09514
Seon <>
2021-06-17 00:31:28 +0200

    Template attrbutes are now maintenaed when extracting a view

--- pxls/template.py
+++ pxls/template.py
@@ -16,6 +16,12 @@ class Template(np.ndarray):
         obj.x = x
         obj.y = y
         return obj
+    
+    def __array_finalize__(self, obj):
+        """Numpy internals."""
+        if obj is None: return
+        self.x = getattr(obj, "x", None)
+        self.y = getattr(obj, "y", None)
 
     @staticmethod
     def parse_url(url: str):
